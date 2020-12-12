a414da20e9a5d5e5b53c6f4d335e8b766759fca1
Yasunari t <u7df25c588m737@gmail.com>
2020-12-12 20:48:31 +0900

    chore: types

---
+++ src/types/prefecture.ts
@@ -0,0 +1,10 @@
+export type Prefecture = {
+  prefCode: number
+  prefName: string
+}
+export type StatePrefectures = Prefecture[]
+
+export type PopulationTransitionData = {
+  year: number
+  value: string
+}
