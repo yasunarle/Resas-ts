633272220716044c33c13dc62a106aa6d14cc581
Yasunari t <u7df25c588m737@gmail.com>
2020-12-12 21:22:00 +0900

    refactor: type checkedPrefecure

--- src/types/prefecture.ts
+++ src/types/prefecture.ts
@@ -8,3 +8,11 @@ export type PopulationTransitionData = {
   year: number
   value: string
 }
+export type Population = string
+
+export type CheckedPrefecture = {
+  prefCode: number
+  prefName: string
+  data: Population[]
+}
+export type CheckedPrefectures = CheckedPrefecture[]
